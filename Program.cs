3e5e72e76d31aae8da7642432657e534fa72ead3
Rupesh80 <57830824+Rupesh80@users.noreply.github.com>
2021-05-27 19:23:57 +0530

    add sample dot net core project
    add sample dot net core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
